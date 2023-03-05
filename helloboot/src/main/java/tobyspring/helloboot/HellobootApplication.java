bb1197fbd26479e84db10ef552e76d48a0bf50c0
KimJinkYU <jinkyu1996@hanmail.net>
2023-03-05 18:15:54 +0900

    Feat: Bean의 생명주기 메소드

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,6 +4,7 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,17 +13,28 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan
 public class HellobootApplication {
+	@Bean
+	public ServletWebServerFactory serverFactory() {
+		return new TomcatServletWebServerFactory();
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
 
 	public static void main(String[] args) {
 		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 			@Override
 			protected void onRefresh() {
 				super.onRefresh();
-				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+				// Application Context를 자동으로 주입해준다.
+//				dispatcherServlet.setApplicationContext(this);
+
 				WebServer webServer = serverFactory.getWebServer(servletContext -> {
-					servletContext.addServlet("dispatcherServlet",
-						new DispatcherServlet(this)
-					).addMapping("/*");
+					servletContext.addServlet("dispatcherServlet", dispatcherServlet).addMapping("/*");
 				});
 				webServer.start();
 			}
