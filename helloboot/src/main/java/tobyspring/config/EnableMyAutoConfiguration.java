e9685c7a930b7de40696bf7e2ede0961ef2e8020
KimJinkYU <jinkyu1996@hanmail.net>
2023-03-12 02:49:53 +0900

    Feat: 스타터와 Jetty 서버 구성 추가

--- helloboot/src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
+++ helloboot/src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -5,7 +5,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.context.annotation.Import;
-import tobyspring.config.autoconfig.MyAutoConfigImportSelector;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
