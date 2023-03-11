1d9f98f67bf95e092674e15a766d8c7a31bc913a
KimJinkYU <jinkyu1996@hanmail.net>
2023-03-11 23:46:27 +0900

    Feat: 인프라 빈 구성 정보의 분리

---
+++ helloboot/src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -0,0 +1,16 @@
+package tobyspring.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.autoconfig.TomcatWebServerConfig;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import({DispatcherServlet.class, TomcatWebServerConfig.class})
+public @interface EnableMyAutoConfiguration {
+
+}
