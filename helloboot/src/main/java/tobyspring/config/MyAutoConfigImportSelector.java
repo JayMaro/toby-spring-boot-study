17c2ace5d359adc986fde53b477496cd350bfbbb
KimJinkYU <jinkyu1996@hanmail.net>
2023-03-20 22:26:15 +0900

    Feat: 자동 구성 정보 대체하기

--- helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ helloboot/src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 import tobyspring.config.MyAutoConfiguration;
 
+// DeferredImportSelector를 구현해서 유저 구성정보가 먼저 로딩이 된 후 자동구성정보가 적용되게 한다.
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
 
     private final ClassLoader classLoader;
