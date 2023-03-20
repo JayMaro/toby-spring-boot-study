47d904058cc698f23d5b1bdf5f178151e0f0fc15
KimJinkYU <jinkyu1996@hanmail.net>
2023-03-21 00:27:11 +0900

    Feat: 환경변수 우선순위 테스트

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -10,22 +10,22 @@ import tobyspring.config.MySpringBootApplication;
 @MySpringBootApplication
 public class HellobootApplication {
 
-	/**
-	 * 우선 순위
-	 * 1. SystemProperty(VM Option)
-	 * 2. EnvironmentVariable
-	 * 3. application.properties(yml)
-	 *
-	 * @param env
-	 * @return
-	 */
-	@Bean
-	ApplicationRunner applicationRunner(Environment env) {
-		return args -> {
-			String name = env.getProperty("my.name");
-			System.out.println("name = " + name);
-		};
-	}
+//	/**
+//	 * 우선 순위
+//	 * 1. SystemProperty(VM Option)
+//	 * 2. EnvironmentVariable
+//	 * 3. application.properties(yml)
+//	 *
+//	 * @param env
+//	 * @return
+//	 */
+//	@Bean
+//	ApplicationRunner applicationRunner(Environment env) {
+//		return args -> {
+//			String name = env.getProperty("my.name");
+//			System.out.println("name = " + name);
+//		};
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
